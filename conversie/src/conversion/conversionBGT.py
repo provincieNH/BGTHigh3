aa5310284fa58203d32936f0ec783fa4ba6d5532
Thomas de Groot <thomas.de.groot@triply.cc>
2020-01-13 17:35:28 +0100

    clean up of conversie folder and start on implementation of the conversion script.

--- conversie/src/conversion/conversionBGT.py
+++ conversie/src/conversion/conversionBGT.py
@@ -1,20 +1,17 @@
 import rdflib
 from .shared import *
-
-def lineToDict(line):
-    # TODO
-    return line
+from .gml_naar_dict import xml_naar_dict
 
 def conversion(dict):
     graph = rdflib.Graph()
     graph.add((URIRef("http://www.example.org#a"),URIRef("http://www.example.org#a"),URIRef("http://www.example.org#a")) )
     return graph
 
-
 def convertBGT():
     lines = ["","","",""]
     file = open("output/output.nt", "ab+")
-    for line in lines:
-        dict = lineToDict(line)
+    bgt_gml ="resources/bgt_begroeidterreindeel.gml"
+
+    for dict in xml_naar_dict(bgt_gml):
         graph = conversion(dict)
         file.write(graph.serialize(format='nt'))
